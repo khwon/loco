fa46d6831664b97dddb563844eddd3a365bf8438
kanghee won <snowkh@gmail.com>
2014-07-31 14:18:12 +0900

    remove FIXME comments

--- spec/termapp/core_ext/string_spec.rb
+++ spec/termapp/core_ext/string_spec.rb
@@ -13,39 +13,6 @@ RSpec.describe String, type: :termapp do
     end
 
     it 'returns 1 for ASCII characters' do
-      # FIXME
-      # 1 => 0
-      # 2 => 2
-      # 3 => 2
-      # 4 => 2
-      # 5 => 2
-      # 6 => 2
-      # 7 => 2
-      # 8 => 0
-      # 9 => 8
-      # 10 => [1, 0]
-      # 11 => 2
-      # 12 => 2
-      # 13 => 0
-      # 14 => 2
-      # 15 => 2
-      # 16 => 2
-      # 17 => 2
-      # 18 => 2
-      # 19 => 2
-      # 20 => 2
-      # 21 => 2
-      # 22 => 2
-      # 23 => 2
-      # 24 => 2
-      # 25 => 2
-      # 26 => 2
-      # 27 => 2
-      # 28 => 2
-      # 29 => 2
-      # 30 => 2
-      # 31 => 2
-      # 127 => 2
       (32..126).each do |ascii|
         @locoterm.mvaddstr(0, 0, ascii.chr)
         expect([ascii, [0, 1]]).to eq([ascii, @locoterm.getyx])
@@ -53,7 +20,6 @@ RSpec.describe String, type: :termapp do
     end
 
     it 'returns the size to pring on Ncurses screen' do
-      # FIXME
       [
         '가',
         '가나',
