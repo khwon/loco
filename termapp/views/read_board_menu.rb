9dd5ce0a0709a5ac07cb3ef315ff842092055adc
kanghee won <snowkh@gmail.com>
2014-07-31 16:26:03 +0900

    skeleton for read_board

---
+++ termapp/views/read_board_menu.rb
@@ -0,0 +1,18 @@
+class ReadBoardMenu < TermApp::View
+  def initialize(*args)
+    super
+  end
+
+  def process
+    term.erase_body
+    if term.current_board
+      term.current_board.post.each_with_index do |x, i|
+        term.mvaddstr(i + 4, 1, "#{format('%8d', x.num)} #{x.title}")
+      end
+    else
+      term.mvaddstr(8, 8, '보드를 먼저 선택해 주세요')
+    end
+    term.getch
+    :loco_menu
+  end
+end
