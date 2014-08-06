3118994269f2b09ac4c5c5063e5a5f4e13c16d05
ChaYoung You <yousbe@gmail.com>
2014-08-07 05:05:39 +0900

    Add test for login
    When logged in it processes WelcomeMenu and LocoMenu.
    
    Close #42

--- spec/termapp/run_spec.rb
+++ spec/termapp/run_spec.rb
@@ -21,6 +21,20 @@ RSpec.describe TermApp, type: :termapp do
       end
     end
 
+    def mock_pw_input(dummy_pw)
+      mocking = true
+      allow(@app.term).to receive(:mvgetnstr).with(
+        21, 40, anything, 20, echo: false
+      ) do |y, x, str, n, echo: false|
+        if mocking
+          mocking = false
+          str.replace(dummy_pw)
+        else
+          original_mvgetnstr.call(y, x, str, n, echo: echo)
+        end
+      end
+    end
+
     before(:example) do
       silence_warnings { @app = TermApp::Application.new }
     end
@@ -36,6 +50,39 @@ RSpec.describe TermApp, type: :termapp do
       expect(cached_processors).to only_have_processors(%i(
         login_menu goodbye_menu
       ))
+    end
+
+    context 'when logged in' do
+      it 'processes WelcomeMenu and LocoMenu' do
+        mock_id_input('testid')
+        mock_pw_input('testpw')
+        user = instance_double('User')
+        allow(User).to receive(:find_by).with(username: 'testid')
+                       .and_return(user)
+        allow(user).to receive(:try).with(:authenticate, 'testpw')
+                       .and_return(user)
+        allow(user).to receive(:admin?).and_return(true)
+        allow(@app.term).to receive(:getch).and_return(
+          # WelcomeMenu
+          Ncurses::KEY_ENTER,
+          # g
+          103,
+          # LocoMenu
+          Ncurses::KEY_ENTER,
+          # GoodbyeMenu
+          Ncurses::KEY_ENTER
+        )
+
+        @app.run
+
+        expect(User).to have_received(:find_by).with(username: 'testid').once
+        expect(user).to have_received(:try).with(:authenticate, 'testpw').once
+
+        cached_processors = @app.instance_variable_get(:@cached_processors)
+        expect(cached_processors).to only_have_processors(%i(
+          login_menu welcome_menu loco_menu goodbye_menu
+        ))
+      end
     end
   end
 end
