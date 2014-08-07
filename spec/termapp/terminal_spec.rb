d824bce10c58ffa6911de625aa5b8568b706a42e
ChaYoung You <yousbe@gmail.com>
2014-08-07 18:50:26 +0900

    Use `around` for entire test suite

--- spec/termapp/terminal_spec.rb
+++ spec/termapp/terminal_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 require 'terminal'
 
 RSpec.describe TermApp::Terminal, type: :termapp do
-  context 'with delegators' do
-    around(:example) do |example|
-      suppress_warnings { @term = TermApp::Terminal.new }
-      example.run
-      @term.terminate
-    end
+  around(:example) do |example|
+    suppress_warnings { @term = TermApp::Terminal.new }
+    example.run
+    @term.terminate
+  end
 
+  context 'with delegators' do
     %i(erase noecho echo beep terminate refresh move getch).each do |method|
       it "responds to #{method}" do
         expect(@term.respond_to? method).to be true
