0d69014749a7a4381e991e4d7c1ec722ceca10b8
ChaYoung You <yousbe@gmail.com>
2014-08-08 02:11:16 +0900

    Update documentation for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password validations: false
 
+  # Check if the User is admin.
+  #
+  # Returns a Boolean whether the User is admin or not.
   def admin?
     # FIXME : implement admin? function
     username == 'a'
