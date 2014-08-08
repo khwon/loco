102a4deba9cb2edb33202ce5deb88050adc7b07f
ChaYoung You <yousbe@gmail.com>
2014-08-09 00:15:21 +0900

    Use built in `respond_to` matcher

--- spec/termapp/terminal_spec.rb
+++ spec/termapp/terminal_spec.rb
@@ -9,14 +9,14 @@ RSpec.describe TermApp::Terminal, type: :termapp do
 
   %i(erase noecho echo beep terminate refresh move getch).each do |method|
     it "responds to #{method}" do
-      expect(subject.respond_to? method).to be true
+      expect(subject).to respond_to(method)
     end
   end
 
   describe '.color_<color>' do
     TermApp::Terminal.const_get(:COLOR_SYMBOLS).each do |color_method|
       it "responds to #{color_method}" do
-        expect(subject.respond_to? color_method).to be true
+        expect(subject).to respond_to(color_method)
       end
     end
   end
