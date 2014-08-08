97d3e0a064a5a76c64523ad4b95e813fefdeb7c5
ChaYoung You <yousbe@gmail.com>
2014-08-09 01:27:27 +0900

    Move helper methods to a module `LoginHelpers`

--- spec/termapp/application_spec.rb
+++ spec/termapp/application_spec.rb
@@ -2,38 +2,41 @@ require 'rails_helper'
 require_relative '../../config/environment'
 require 'application'
 
+module LoginHelpers
+  def mock_id_input(dummy_id)
+    mocking = true
+    allow(subject.term).to receive(:mvgetnstr).with(
+      20, 40, anything, 20
+    ) do |y, x, str, n|
+      if mocking
+        mocking = false
+        str.replace(dummy_id)
+      else
+        original_mvgetnstr.call(y, x, str, n)
+      end
+    end
+  end
+
+  def mock_pw_input(dummy_pw)
+    mocking = true
+    allow(subject.term).to receive(:mvgetnstr).with(
+      21, 40, anything, 20, echo: false
+    ) do |y, x, str, n, echo: false|
+      if mocking
+        mocking = false
+        str.replace(dummy_pw)
+      else
+        original_mvgetnstr.call(y, x, str, n, echo: echo)
+      end
+    end
+  end
+end
+
 RSpec.describe TermApp::Application, type: :termapp do
   include TermAppHelpers
+  include LoginHelpers
 
   describe '.run' do
-    def mock_id_input(dummy_id)
-      mocking = true
-      allow(subject.term).to receive(:mvgetnstr).with(
-        20, 40, anything, 20
-      ) do |y, x, str, n|
-        if mocking
-          mocking = false
-          str.replace(dummy_id)
-        else
-          original_mvgetnstr.call(y, x, str, n)
-        end
-      end
-    end
-
-    def mock_pw_input(dummy_pw)
-      mocking = true
-      allow(subject.term).to receive(:mvgetnstr).with(
-        21, 40, anything, 20, echo: false
-      ) do |y, x, str, n, echo: false|
-        if mocking
-          mocking = false
-          str.replace(dummy_pw)
-        else
-          original_mvgetnstr.call(y, x, str, n, echo: echo)
-        end
-      end
-    end
-
     subject { silence_warnings { TermApp::Application.new } }
     let!(:original_mvgetnstr) { subject.term.method(:mvgetnstr) }
 
