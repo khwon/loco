95086616cb70e3c819bb43eed3ec39d3d895e84d
ChaYoung You <yousbe@gmail.com>
2014-08-09 19:47:24 +0900

    Add `SelectBoardMenu#print_boards`
    Add `cur_boards` instance variable.

--- termapp/processors/select_board_menu.rb
+++ termapp/processors/select_board_menu.rb
@@ -3,35 +3,25 @@
 class SelectBoardMenu < TermApp::Processor
   def process
     str = ''
-    cur_boards = []
+    @cur_boards = []
     selected = []
     loop do
       # TODO : consider setting start point as locoterm.current_board
-      cur_boards = [] if str == ''
-      term.erase_body
+      @cur_boards = [] if str == ''
       if selected.last.nil? || selected.last.is_dir
         list = Board.get_list(parent_board: selected.last)
       else
         list = Board.get_list(parent_board: selected[-2])
       end
-      list.each_with_index do |x, i|
-        # TODO : redraw only changed lines
-        if x == cur_boards.first
-          term.color_black(reverse: true) do
-            term.mvaddstr(i + 4, 3, x.path_name)
-          end
-        else
-          term.mvaddstr(i + 4, 3, x.path_name)
-        end
-      end
+      print_boards(list)
       term.mvaddstr(3, 1, "select board : #{str}")
       term.refresh
       c = term.getch
       term.clrtoeol(3)
       case c
       when 9, 32 # tab, space
-        if cur_boards.size == 1 && str != cur_boards.first.path_name
-          selected << cur_boards.first
+        if @cur_boards.size == 1 && str != @cur_boards.first.path_name
+          selected << @cur_boards.first
           str = selected.last.path_name
         else
           term.beep
@@ -39,9 +29,9 @@ class SelectBoardMenu < TermApp::Processor
       when 27 # esc
         break :loco_menu
       when Ncurses::KEY_ENTER, 10 # enter
-        if cur_boards.size == 1 && !cur_boards.first.is_dir
+        if @cur_boards.size == 1 && !@cur_boards.first.is_dir
           # TODO : for now, only allow selecting non-dir boards
-          term.current_board = cur_boards.first
+          term.current_board = @cur_boards.first
           # TODO : for now, return to loco menu
           break :loco_menu
         else
@@ -49,7 +39,7 @@ class SelectBoardMenu < TermApp::Processor
         end
       when 127, Ncurses::KEY_BACKSPACE
         if selected.size > 0 && str == selected.last.path_name
-          cur_boards = [selected[-1]]
+          @cur_boards = [selected[-1]]
           selected = selected[0..-2]
         end
         str = str[0..-2]
@@ -57,11 +47,30 @@ class SelectBoardMenu < TermApp::Processor
         next unless c < 127
         matched = list.select { |x| x.path_name.starts_with? str + c.chr }
         if matched.size > 0
-          cur_boards = matched
+          @cur_boards = matched
           str += c.chr
         else
           term.beep
         end
+      end
+    end
+  end
+
+  # Print given Board list. Current Board is printed in reversed color.
+  #
+  # list - The Array of Board to print.
+  #
+  # Returns nothing.
+  def print_boards(list)
+    term.erase_body
+    list.each_with_index do |x, i|
+      # TODO : redraw only changed lines
+      if x == @cur_boards.first
+        term.color_black(reverse: true) do
+          term.mvaddstr(i + 4, 3, x.path_name)
+        end
+      else
+        term.mvaddstr(i + 4, 3, x.path_name)
       end
     end
   end
