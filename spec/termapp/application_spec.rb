55c1a5d1ced3e8c6b7a0c369d1a9ab4b4fb5399b
ChaYoung You <yousbe@gmail.com>
2014-08-09 13:57:13 +0900

    Use named subject

--- spec/termapp/application_spec.rb
+++ spec/termapp/application_spec.rb
@@ -5,8 +5,8 @@ require 'application'
 module LoginHelpers
   def mock_id_input(dummy_id)
     mocking = true
-    allow(subject.term).to receive(:mvgetnstr).with(
-                             20, 40, anything, 20) do |y, x, str, n|
+    allow(app.term).to receive(:mvgetnstr).with(
+                         20, 40, anything, 20) do |y, x, str, n|
       if mocking
         mocking = false
         str.replace(dummy_id)
@@ -18,9 +18,9 @@ module LoginHelpers
 
   def mock_pw_input(dummy_pw)
     mocking = true
-    allow(subject.term).to receive(:mvgetnstr).with(
-                             21, 40, anything, 20, echo: false
-                           ) do |y, x, str, n, echo: false|
+    allow(app.term).to receive(:mvgetnstr).with(
+                         21, 40, anything, 20, echo: false
+                       ) do |y, x, str, n, echo: false|
       if mocking
         mocking = false
         str.replace(dummy_pw)
@@ -36,19 +36,19 @@ RSpec.describe TermApp::Application, type: :termapp do
   include LoginHelpers
 
   describe '.run' do
-    subject { silence_warnings { TermApp::Application.new } }
-    let!(:original_mvgetnstr) { subject.term.method(:mvgetnstr) }
+    subject(:app) { silence_warnings { TermApp::Application.new } }
+    let!(:original_mvgetnstr) { app.term.method(:mvgetnstr) }
 
     it "processes GoodbyeMenu when get 'off' as id" do
       mock_id_input('off')
       # GoodbyeMenu
-      allow(subject.term).to receive(:getch) { Ncurses::KEY_ENTER }
-      subject.run
+      allow(app.term).to receive(:getch) { Ncurses::KEY_ENTER }
+      app.run
 
-      expect(subject.term).to have_received_id.once
-      expect(subject.term).to have_received(:getch).with(no_args).once
+      expect(app.term).to have_received_id.once
+      expect(app.term).to have_received(:getch).with(no_args).once
 
-      cached_processors = subject.instance_variable_get(:@cached_processors)
+      cached_processors = app.instance_variable_get(:@cached_processors)
       expect(cached_processors).to only_have_processors(
                                      %i(
                                        login_menu
@@ -67,30 +67,30 @@ RSpec.describe TermApp::Application, type: :termapp do
         allow(user).to receive(:try).with(:authenticate, user.password)
                                     .and_call_original
         allow(user).to receive(:admin?).and_call_original
-        allow(subject.term).to receive(:getch).and_return(
-                                 # WelcomeMenu
-                                 Ncurses::KEY_ENTER,
-                                 # g
-                                 103,
-                                 # LocoMenu
-                                 Ncurses::KEY_ENTER,
-                                 # GoodbyeMenu
-                                 Ncurses::KEY_ENTER
-                               )
+        allow(app.term).to receive(:getch).and_return(
+                             # WelcomeMenu
+                             Ncurses::KEY_ENTER,
+                             # g
+                             103,
+                             # LocoMenu
+                             Ncurses::KEY_ENTER,
+                             # GoodbyeMenu
+                             Ncurses::KEY_ENTER
+                           )
 
-        subject.run
+        app.run
 
-        expect(subject.term).to have_received_id.once
-        expect(subject.term).to have_received_pw.once
+        expect(app.term).to have_received_id.once
+        expect(app.term).to have_received_pw.once
         expect(User).to have_received(:find_by).with(username: user.username)
                                                .once
         expect(user).to have_received(:try).with(:authenticate, user.password)
                                            .once
         expect(user).to have_received(:admin?).with(no_args).once
-        expect(subject.term).to have_received(:getch).with(no_args)
-                                                     .exactly(4).times
+        expect(app.term).to have_received(:getch).with(no_args)
+                                                 .exactly(4).times
 
-        cached_processors = subject.instance_variable_get(:@cached_processors)
+        cached_processors = app.instance_variable_get(:@cached_processors)
         expect(cached_processors).to only_have_processors(
                                        %i(
                                          login_menu
