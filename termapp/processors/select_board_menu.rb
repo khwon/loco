2f1eb8251fbfb60ced58a2292a2ac873db812e95
ChaYoung You <yousbe@gmail.com>
2014-08-09 23:15:16 +0900

    Refresh only changed line in `SelectBoardMenu`

--- termapp/processors/select_board_menu.rb
+++ termapp/processors/select_board_menu.rb
@@ -4,10 +4,15 @@ class SelectBoardMenu < TermApp::Processor
   def process
     str = ''
     @cur_boards = []
+    @past_boards = nil
+    @list = nil
     selected = []
     loop do
       # TODO : consider setting start point as locoterm.current_board
-      @cur_boards = [] if str == ''
+      if str == ''
+        @past_boards = @cur_boards
+        @cur_boards = []
+      end
       list = if selected.last.nil? || selected.last.is_dir
                Board.get_list(parent_board: selected.last)
              else
@@ -35,35 +40,51 @@ class SelectBoardMenu < TermApp::Processor
         break :loco_menu
       when 127, Ncurses::KEY_BACKSPACE
         if selected.size > 0 && str == selected.last.path_name
+          @past_boards = @cur_boards
           @cur_boards = [selected.pop]
         end
         str = str[0..-2]
       else
         next unless c < 127
-        matched = list.select { |x| x.path_name.starts_with? str + c.chr }
+        matched = @list.select { |x| x.path_name.starts_with? str + c.chr }
         next term.beep unless matched.size > 0
+        @past_boards = @cur_boards
         @cur_boards = matched
         str += c.chr
       end
     end
   end
 
-  # Print given Board list. Current Board is printed in reversed color.
+  # Print given Board list. Current Board is printed in reversed color. Refresh
+  # only changed lines if previous list exists and is same with passed list.
   #
   # list - The Array of Board to print.
   #
   # Returns nothing.
   def print_boards(list)
-    term.erase_body
-    list.each_with_index do |x, i|
-      # TODO : redraw only changed lines
-      if x == @cur_boards.first
+    if @list && @list == list
+      past_board = @list.index(@past_boards.first)
+      cur_board = @list.index(@cur_boards.first)
+      if past_board
+        term.mvaddstr(past_board + 4, 3, @past_boards.first.path_name)
+      end
+      if cur_board
         term.color_black(reverse: true) do
+          term.mvaddstr(cur_board + 4, 3, @cur_boards.first.path_name)
+        end
+      end
+    else
+      term.erase_body
+      list.each_with_index do |x, i|
+        if x == @cur_boards.first
+          term.color_black(reverse: true) do
+            term.mvaddstr(i + 4, 3, x.path_name)
+          end
+        else
           term.mvaddstr(i + 4, 3, x.path_name)
         end
-      else
-        term.mvaddstr(i + 4, 3, x.path_name)
       end
+      @list = list
     end
   end
 end
