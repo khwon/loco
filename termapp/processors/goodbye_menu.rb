548a28a4be4132e74c09b8ffa0b206ef8f153aee
ChaYoung You <yousbe@gmail.com>
2014-08-24 17:31:24 +0900

    Refresh screen explicitly in menus

--- termapp/processors/goodbye_menu.rb
+++ termapp/processors/goodbye_menu.rb
@@ -4,6 +4,7 @@ class GoodbyeMenu < TermApp::Processor
   def process
     term.erase_all
     term.mvaddstr(5, 5, 'goodbye!')
+    term.refresh
     term.getch
     nil
   end
--- termapp/processors/welcome_menu.rb
+++ termapp/processors/welcome_menu.rb
@@ -4,6 +4,7 @@ class WelcomeMenu < TermApp::Processor
   def process
     term.erase
     term.mvaddstr(15, 30, 'welcome to new loco!')
+    term.refresh
     term.getch
     :loco_menu
   end
