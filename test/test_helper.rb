4480ef6e6203ec191a0d3d1bd310647db2f057f3
ChaYoung You <yousbe@gmail.com>
2014-08-26 17:04:03 +0900

    Update documentation
    - `ApplicationController`
    - `ApplicationHelper`
    - `ActiveSupport::TestCase`

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 module ActiveSupport
+  # Configuration for test.
   class TestCase
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
     # order.
