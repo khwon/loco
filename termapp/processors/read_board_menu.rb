a72489799968eb28276fa33c9a4fa05cc007e66f
kanghee won <snowkh@gmail.com>
2014-08-31 15:55:33 +0900

    improve readboardmenu

--- termapp/processors/read_board_menu.rb
+++ termapp/processors/read_board_menu.rb
@@ -7,12 +7,13 @@ class ReadBoardMenu < TermApp::Processor
     if cur_board
       posts = cur_board.post.order('num desc').limit(term.lines - 5)
       posts.reverse.each_with_index do |x, i|
-        str = format('%8d', x.num)
-        str << format(' %13s', x.writer.nickname)
-        str << '??' # Date
-        str << '??' # View count
-        str << x.title
-        term.mvaddstr(i + 4, 1, str)
+        strs = []
+        strs << format('%6d', x.num)
+        strs << format('%-12s', x.writer.nickname)
+        strs << x.created_at.strftime('%m/%d') # Date
+        strs << '????' # View count
+        strs << x.title.unicode_slice(term.columns-32)
+        term.mvaddstr(i + 4, 1, strs.join(' '))
       end
     else
       term.mvaddstr(8, 8, '보드를 먼저 선택해 주세요')
