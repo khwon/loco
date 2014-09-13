6f76c4c3997266a120646a6ffa356fbd44435d1d
ChaYoung You <yousbe@gmail.com>
2014-09-14 05:40:23 +0900

    Move `LoginHelpers` to separate file

--- spec/termapp/application_spec.rb
+++ spec/termapp/application_spec.rb
@@ -2,35 +2,6 @@ require 'rails_helper'
 require_relative '../../config/environment'
 require 'application'
 
-# Helper methods for login process test of TermApp::Application.
-module LoginHelpers
-  def mock_id_input(term, dummy_id)
-    mocking = true
-    allow(term).to receive(:mvgetnstr).with(
-                     20, 40, anything, 20) do |y, x, str, n|
-      if mocking
-        mocking = false
-        str.replace(dummy_id)
-      else
-        original_mvgetnstr.call(y, x, str, n)
-      end
-    end
-  end
-
-  def mock_pw_input(term, dummy_pw)
-    mocking = true
-    allow(term).to receive(:mvgetnstr)
-      .with(21, 40, anything, 20, echo: false) do |y, x, str, n, echo: false|
-      if mocking
-        mocking = false
-        str.replace(dummy_pw)
-      else
-        original_mvgetnstr.call(y, x, str, n, echo: echo)
-      end
-    end
-  end
-end
-
 RSpec.describe TermApp::Application, type: :termapp do
   include TermAppHelpers
   include LoginHelpers
