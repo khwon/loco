633fdca6f162d5ecf724459e92b47008fea38473
ChaYoung You <yousbe@gmail.com>
2014-09-25 00:29:13 +0900

    Validate presence and uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # User information of Loco.
 class User < ActiveRecord::Base
   has_secure_password validations: false
+  validates :username, presence: true, uniqueness: true
 
   # Check if the User is admin.
   #
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  it 'is a valid factory' do
+    expect(user).to be_valid
+    expect(user.save).to be true
+  end
+
+  it 'should not be created with an empty username' do
+    [nil, ''].each do |username|
+      user.username = username
+      expect(user).not_to be_valid
+      expect(user.errors.keys).to include(:username)
+    end
+  end
+
+  it 'should not be created with an duplicated username' do
+    duplicated_user = user.dup
+    user.save!
+    expect(duplicated_user).not_to be_valid
+    expect(duplicated_user.errors.keys).to include(:username)
+  end
+
+  it 'should be created with an unique username' do
+    user.save!
+    another_user = build(:user)
+    expect(another_user).to be_valid
+    expect(another_user.save).to be true
+  end
 end
