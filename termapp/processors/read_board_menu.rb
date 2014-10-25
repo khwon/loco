e1888802b4b9b4193c5a541aec4e8e3b87b83a03
ChaYoung You <yousbe@gmail.com>
2014-10-25 19:54:12 +0900

    Avoid refreshing terminal if the index is same

--- termapp/processors/read_board_menu.rb
+++ termapp/processors/read_board_menu.rb
@@ -16,7 +16,6 @@ module TermApp
           @cur_index = @num_lists - 1
         end
         print_posts
-        term.refresh
         @past_index = @cur_index
         control, *args = process_key(term.getch)
         case control
@@ -140,7 +139,8 @@ module TermApp
           term.mvaddstr(i + 4, 0, ' ' + post.format_for_term(term.columns - 32))
           term.color_black # reset color
         end
-      elsif @past_index != @cur_index
+      else
+        return if @past_index == @cur_index
         past_line = @posts[@past_index].format_for_term(term.columns - 32)
         cur_line = @posts[@cur_index].format_for_term(term.columns - 32)
         term.mvaddstr(@past_index + 4, 0, ' ' + past_line)
@@ -149,6 +149,7 @@ module TermApp
         end
       end
       term.mvaddstr(@cur_index + 4, 0, '>')
+      term.refresh
     end
   end
 end
