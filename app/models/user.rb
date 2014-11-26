0b27491e79d5234cede14c61945ea9a7a32fd9d8
kangheui won <snowkh@gmail.com>
2014-11-26 15:12:03 +0800

    support old-style crypt password

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,21 @@ class User < ActiveRecord::Base
     # FIXME: Implement admin? function.
     username == 'a'
   end
+
+  def auth pw
+    if old_crypt_password
+      if pw.crypt(old_crypt_password[0..1]) ==
+        old_crypt_password
+        self.password = pw
+        self.old_crypt_password = nil
+        save!
+        self
+      else
+        false
+      end
+    else
+      authenticate(pw)
+    end
+  end
+
 end
