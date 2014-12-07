d55def34c9f4a9c37189585ec9bfecc025ae1ea4
ChaYoung You <yousbe@gmail.com>
2014-12-07 18:19:29 +0900

    Add tests for `Board.get_list`

--- spec/factories/boards.rb
+++ spec/factories/boards.rb
@@ -5,6 +5,16 @@ FactoryGirl.define do
     parent_id nil
     is_dir true
     sequence(:name) { |n| "continent#{n}" }
+
+    factory :board_with_children do
+      transient do
+        child_count 5
+      end
+
+      after(:create) do |board, evaluator|
+        create_list(:child_board, evaluator.child_count, parent: board)
+      end
+    end
   end
 
   factory :child_board, class: Board do
