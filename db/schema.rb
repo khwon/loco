079cdc507f6b4f45113b295d406ee5506305515a
kangheui won <snowkh@gmail.com>
2014-12-07 16:52:47 +0900

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140731071942) do
+ActiveRecord::Schema.define(version: 20141207071236) do
 
   create_table "boards", force: true do |t|
     t.integer  "owner_id"
