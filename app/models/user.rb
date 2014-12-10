0cf2b3f5e511ba0679d7cdded1f224bbdd6c223c
kangheui won <snowkh@gmail.com>
2014-12-10 11:01:58 +0900

    remove validation in username
    there is username with [:whitespace:]+

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # User information of Loco.
 class User < ActiveRecord::Base
   has_secure_password validations: false
-  validates :username, presence: true, uniqueness: true
+  validates :username, uniqueness: true
 
   # Check if the User is admin.
   #
