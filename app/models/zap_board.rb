2ef33487ecddf0f42ed9dec89a80e72318ce1bcf
ChaYoung You <yousbe@gmail.com>
2015-01-14 14:15:33 +0900

    Update documentation

--- app/models/zap_board.rb
+++ app/models/zap_board.rb
@@ -1,7 +1,14 @@
+# Holds zapped Boards of User. Belongs to User and Board.
 class ZapBoard < ActiveRecord::Base
   belongs_to :user
   belongs_to :board
 
+  # Zap the Board from the User.
+  #
+  # board - The Board to zap.
+  # user  - The User zapping.
+  #
+  # Returns nothing.
   def self.zap(board, user)
     model = ZapBoard.new
     model.board = board
