777a6e494a5d7ae6850251fb8ad075868454686b
kangheui won <snowkh@gmail.com>
2015-02-03 17:51:43 +0900

    refactored readnew

--- termapp/processors/read_new_menu.rb
+++ termapp/processors/read_new_menu.rb
@@ -4,35 +4,17 @@ module TermApp
     def process
       term.noecho
       flash = nil
-      leaves = Board.each_leaves_for_newread(term.current_user)
-      vr_max_cache = {}
-      VisitreadMax.where(board_id: leaves.map(&:id)).each do |x|
-        vr_max_cache[x.board_id] = x.num
-      end
-      max_num_cache = {}
-      Post.select('board_id, max(num) as max_num').group(:board_id)
-        .having(board_id: leaves.map(&:id)).each do |x|
-        max_num_cache[x[:board_id]] = x[:max_num]
-      end
-      leaves.each do |b|
+      started_time = Time.now.to_f
+      init_process
+      @leaves.each do |b|
         # next if b.zapped_by? term.current_user
         orig_board = b
         b = b.alias_board while b.alias_board
-        # max_num = max_num_cache[b.id] || b.max_num
-        max_num = max_num_cache[b.id] || 0
-        # max_num = b.max_num
-        vr_max = nil
-        if vr_max_cache.key? orig_board.id
-          if max_num == vr_max_cache[orig_board.id]
-            next
-          else
-            vr_max = VisitreadMax.find_by(user_id: term.current_user.id,
-                                          board_id: orig_board.id)
-          end
-        else
-          vr_max = VisitreadMax.create!(user: term.current_user,
-                                        board: orig_board)
-        end
+        max_num = @max_num_cache[b.id] || 0
+        next if max_num == (@vr_max_cache[orig_board.id] || 0)
+        @checked_board_cnt += 1
+        vr_max = VisitreadMax.find_or_create_by!(user_id: term.current_user.id,
+                                                 board_id: orig_board.id)
         list = BoardRead.where(user: term.current_user)
                .where(board: b).order('lower(posts)').to_a
         num = 1
@@ -42,6 +24,7 @@ module TermApp
             list.shift
             next
           end
+          @time_spent = nil
           post = b.post.find_by(num: num)
           term.erase_body
           strs = []
@@ -62,6 +45,7 @@ module TermApp
           control, *args = process_key(key)
           case control
           when :quit
+            print_stop
             term.echo
             return args
           when :zap
@@ -78,6 +62,7 @@ module TermApp
           when :read
             case read_post(post)
             when :quit
+              print_stop
               term.echo
               return :loco_menu
             end
@@ -89,8 +74,8 @@ module TermApp
         vr_max.num = num - 1
         vr_max.save!
       end
-      term.erase_body
-      term.get_wch
+      @time_spent = Time.now.to_f - started_time if @time_spent
+      print_end
       term.echo
       :loco_menu
     end
@@ -118,6 +103,43 @@ module TermApp
       when :q then :quit
       else nil
       end
+    end
+
+    def init_process
+      @time_spent = 0
+      @checked_board_cnt = 0
+      @leaves = Board.each_leaves_for_newread(term.current_user)
+      @vr_max_cache = {}
+      VisitreadMax.where(board_id: @leaves.map(&:id)).each do |x|
+        @vr_max_cache[x.board_id] = x.num
+      end
+      @max_num_cache = {}
+      Post.select('board_id, max(num) as max_num').group(:board_id)
+        .having(board_id: @leaves.map(&:id)).each do |x|
+        @max_num_cache[x[:board_id]] = x[:max_num]
+      end
+    end
+
+    def print_end
+      term.erase_body
+      if @time_spent
+        term.mvaddstr(3, 3, '* 새 글이 없습니다.')
+        term.mvaddstr(4, 5, '조금 후에 다시 시도하세요.')
+        term.mvaddstr(5, 5, "소요 시간 : #{@time_spent}초")
+      else
+        term.mvaddstr(3, 3, '* 새 글 읽기가 끝났습니다.')
+        term.mvaddstr(4, 5, '수고 하셨습니다 (__);')
+      end
+      term.mvaddstr(7, 5, "전체 보드 수 : #{Board.where(is_dir: false).count}")
+      term.mvaddstr(8, 5, "새글을 확인한 보드 수 : #{@checked_board_cnt}")
+      term.mvaddstr(9, 5, '(이외의 보드에서는 새글이 없는 것이 확실하여 확인하지 않음)')
+      term.get_wch
+    end
+
+    def print_stop
+      term.erase_body
+      term.mvaddstr(4, 3, '* 새 글 읽기를 중간에 멈춥니다.')
+      term.get_wch
     end
   end
 end
