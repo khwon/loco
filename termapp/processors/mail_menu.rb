0efa6382f239fb3c78cdfe51ea1b30548f584351
kangheui won <snowkh@gmail.com>
2015-08-13 18:44:27 +0900

    init mail_menu

---
+++ termapp/processors/mail_menu.rb
@@ -0,0 +1,14 @@
+require_relative 'loco_menu'
+module TermApp
+  # Corresponding Processor of MenuItem.new('Mail', :mail_menu).
+  class MailMenu < LocoMenu
+    def initialize(*args)
+      super
+      @items.concat [MenuItem.new('Send', :mail_send_menu),
+                     MenuItem.new('Read', :mail_read_menu),
+                     MenuItem.new('Outbox', :mail_outbox_menu),
+                     MenuItem.new('Exit', :main_menu)]
+      @items.freeze
+    end
+  end
+end
